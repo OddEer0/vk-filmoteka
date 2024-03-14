edb08cc0b64a535370598ee0b6c3eb9db3f56eeb
Greenpools Ryan <greenpoo@at-l1.msk.21-school.ru>
2024-03-14 19:54:13 +0300

    [docs/86bxwgqj1]: fix main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -19,3 +19,10 @@ func main() {
 		log.Fatal("Error starting server")
 	}
 }
+
+func initSwagger(mux *http.ServeMux) {
+	mux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui/", http.FileServer(http.Dir("./swagger-ui/"))))
+	mux.HandleFunc("/docs/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/swagger.json")
+	})
+}
