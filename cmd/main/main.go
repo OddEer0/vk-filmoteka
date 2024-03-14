ca36757067b05f8aaa4bf16dc59d5ee819c584bf
Greenpools Ryan <greenpoo@at-l1.msk.21-school.ru>
2024-03-14 15:06:12 +0300

    [feature/86bxwgnxw]: add swagger, and main setup server

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	_ "github.com/OddEer0/ck-filmoteka/docs"
+	"github.com/OddEer0/ck-filmoteka/internal/presentation/router"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
+	server := http.Server{Addr: "localhost:5000", Handler: &router.AppRouter{}}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Error starting server")
+	}
 }
--- internal/presentation/router/router.go
+++ internal/presentation/router/router.go
@@ -5,7 +5,9 @@ import (
 	"strings"
 )
 
-func AppRouter(res http.ResponseWriter, req *http.Request) {
+type AppRouter struct{}
+
+func (r *AppRouter) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	switch {
 	case strings.HasPrefix(req.URL.Path, HttpV1Prefix):
 		HttpV1Router(res, req)
