0751851092cac1fa70b742230ae0c2a067c379b9
Greenpools Ryan <greenpoo@at-l1.msk.21-school.ru>
2024-03-14 19:53:00 +0300

    [docs/86bxwgqj1]: fix main, router

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/OddEer0/ck-filmoteka/docs"
-	"github.com/OddEer0/ck-filmoteka/internal/presentation/router"
+	appRouter "github.com/OddEer0/ck-filmoteka/internal/presentation/router"
 	"log"
 	"net/http"
 )
@@ -11,16 +11,10 @@ import (
 // @version 1.0
 // @description This is a sample HTTP package with Swagger annotations.
 func main() {
-	mux := http.NewServeMux()
+	router := appRouter.NewAppRouter()
+	initSwagger(router)
 
-	mux.HandleFunc("/", router.NewAppRouter)
-
-	mux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui/", http.FileServer(http.Dir("./swagger-ui/"))))
-	mux.HandleFunc("/docs/swagger.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./docs/swagger.json")
-	})
-
-	server := http.Server{Addr: "localhost:5000", Handler: mux}
+	server := http.Server{Addr: "localhost:5000", Handler: router}
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal("Error starting server")
 	}
--- internal/presentation/router/router.go
+++ internal/presentation/router/router.go
@@ -5,11 +5,17 @@ import (
 	"strings"
 )
 
-func NewAppRouter(res http.ResponseWriter, req *http.Request) {
-	switch {
-	case strings.HasPrefix(req.URL.Path, HttpV1Prefix):
-		HttpV1Router(res, req)
-	default:
-		http.NotFound(res, req)
-	}
+func NewAppRouter() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		switch {
+		case strings.HasPrefix(req.URL.Path, HttpV1Prefix):
+			HttpV1Router(res, req)
+		default:
+			http.NotFound(res, req)
+		}
+	})
+
+	return mux
 }
