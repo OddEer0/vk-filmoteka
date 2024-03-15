ec598c18eb61098092dcaf7725e5541c848ffc15
Greenpools Ryan <greenpoo@mi-m3.msk.21-school.ru>
2024-03-15 16:48:17 +0300

    [feature/86bxwgpgx]: add auth use case and services

---
+++ internal/app/services/user_service/user.go
@@ -0,0 +1,22 @@
+package userService
+
+import (
+	"context"
+	appDto "github.com/OddEer0/ck-filmoteka/internal/app/app_dto"
+	"github.com/OddEer0/ck-filmoteka/internal/domain/aggregate"
+	"github.com/OddEer0/ck-filmoteka/internal/domain/repository"
+)
+
+type (
+	Service interface {
+		Create(ctx context.Context, data appDto.RegistrationUseCaseDto) (*aggregate.UserAggregate, error)
+	}
+
+	userService struct {
+		repository.UserRepository
+	}
+)
+
+func New(userRepo repository.UserRepository) Service {
+	return &userService{UserRepository: userRepo}
+}
--- internal/domain/aggregate/user_aggregate.go
+++ internal/domain/aggregate/user_aggregate.go
@@ -19,6 +19,17 @@ func (u *UserAggregate) Validation() error {
 	return nil
 }
 
+func (u *UserAggregate) SetToken(refreshToken string) error {
+	token := model.Token{Id: u.User.Id, Value: refreshToken}
+	validator := appValidator.New()
+	err := validator.Struct(token)
+	if err != nil {
+		return err
+	}
+	u.Token = &token
+	return nil
+}
+
 func NewUserAggregate(user model.User) (*UserAggregate, error) {
 	result := &UserAggregate{User: user}
 	if err := result.Validation(); err != nil {
